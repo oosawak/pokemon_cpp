432932c8a7389be3594e7dbd9b22b205ce24a4f9
oosawak <oosawak@users.noreply.github.com>
2025-06-17 11:01:01 +0900

    Update pokemon001.cpp
    コンストラクタでポケモンのパラメータを初期化するように修正

--- pokemon001.cpp
+++ pokemon001.cpp
@@ -1,56 +1,69 @@
 #include <string>
-
+#include <iostream>
 class Pokemon {
 public:
+	Pokemon();
+	Pokemon(std::string name,int level,int type,int hp,int atk, int def,int special_atk,int special_def,int agi) ;	
+	~Pokemon();
 	int GetAtack(int waza);
 	int SetAtack(int atack_point);
+	std::string getName() {
+		return name;
+	}
+	int getLevel() {
+		return level;
+	}
 private:
 	std::string name;	// 文字列
-	int level;			// レベル
-	int type;			// 属性
-	int hp;				// HP
-	int atk;			// こうげぎ
-	int def;			// 防御
+	int level;		// レベル
+	int type;		// 属性
+	int hp;			// HP
+	int atk;		// こうげぎ
+	int def;		// 防御
 	int special_atk;	// とくこう
 	int special_def;	// とくぼう
-	int agi;			// すばやさ
-	int waza[];			// 技リスト
+	int agi;		// すばやさ
+	int waza[];		// 技リスト
 };
-
 // コンストラクタ
 Pokemon::Pokemon() {
-	this->name = "フロウ";
-	this->level = 29;
-	this->hp = 183;
-	this->atk = 43;
-	this->def = 39;
-	this->special_atk = 46;
-	this->special_def = 39;
-	this->agi = 49;
+	this->name = "フロウ"; this->level = 29; this->hp = 183; this->atk = 43; this->def = 39;
+	this->special_atk = 46; this->special_def = 39; this->agi = 49;
 }
-
+Pokemon::Pokemon(std::string name,int level,int type,int hp,int atk, int def,int special_atk,int special_def,int agi) {
+	this->name = name;
+	this->level = level;
+	this->hp = hp;
+	this->atk = atk;
+	this->def = def;
+	this->special_atk = special_atk;
+	this->special_def = special_atk;
+	this->agi = agi;
+}
 // デストラクタ
 Pokemon::~Pokemon() {
-
 }
-
-
 int Pokemon::GetAtack(int waza) {
 	return atk*(waza/100+1);
 }
-
 // 攻撃値を引数として呼び出すと戻り値でHPを返す
 int Pokemon::SetAtack(int atack_point) {
 	int buttle_point = atack_point - this->def;
 	if(buttle_point>0) {
-		this->hp - buttle_point;
+		this->hp -= buttle_point;
 	}
 	return this->hp;
 }
 
 int main() {
+	// coutの実験
+	std::cout << "ポケモンバトル開始" << std::endl;
+
 	// ポケモンAオブジェクトを生成
-	Pokemon pokemon_a;
+	Pokemon pokemon_a = Pokemon("フロウ",30,1,183,43,39,46,39,49);
+	std::cout << "ポケモンAのステータス" << std::endl
+		<< "名前:" << pokemon_a.getName() << std::endl	// 文字列
+		<< "レベル:" << pokemon_a.getLevel() << std::endl;	// 文字列
 
 	// ポケモンBオブジェクトを生成
 	Pokemon pokemon_b;
